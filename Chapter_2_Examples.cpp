3ba672a20a63e3a9cd3b57802a5b1437db52faf8
aslihancelik <aslihancelik27@gmail.com>
2024-09-07 23:18:39 -0400

    The MONTHLYSALARY constant name is updated to MONTHLY_SALARY .  The comment at the top , explaining the purpose of this program  is expanded.

--- Chapter_2_Examples.cpp
+++ Chapter_2_Examples.cpp
@@ -1,19 +1,21 @@
-//This program calculates different percentages of a salary and 
-// displays the calculated values.
+//This program displays different contributions to the retirement 
+// plan by calculating different percentages of a salary and 
+// displaying the calculated values.
+
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    const double MONTHLYSALARY = 6000.0;
+    const double MONTHLY_SALARY = 6000.0;
     const double PERCENT1 = 5.0, PERCENT2 = 7.0, PERCENT3 = 10.0;
 
     double contribution1, contribution2, contribution3;
 
     //Calculate the contributions to the retirement plan for each percentage
-    contribution1 = MONTHLYSALARY * PERCENT1 / 100.0;
-    contribution2 = MONTHLYSALARY * PERCENT2 / 100.0;
-    contribution3 = MONTHLYSALARY * PERCENT3 / 100.0;
+    contribution1 = MONTHLY_SALARY * PERCENT1 / 100.0;
+    contribution2 = MONTHLY_SALARY * PERCENT2 / 100.0;
+    contribution3 = MONTHLY_SALARY * PERCENT3 / 100.0;
 
     cout << "The amount of contribution to the retirement plan at " << PERCENT1
         << "% is $" << contribution1 << " ." << endl;
